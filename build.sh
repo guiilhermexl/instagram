d7293d170ac05cf63391e907f651fa0b2c7060eb
guiilhermexl <guilhermebrazilgames@gmail.com>
2025-05-28 20:09:20 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get update
-apt-get install -y wget unzip xvfb
+sudo apt-get update
+sudo apt-get install -y wget unzip xvfb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
 pip install -r requirements.txt
