148d437494f7cf1b2464d2f38804fb6dc0862815
guiilhermexl <guilhermebrazilgames@gmail.com>
2025-05-28 21:18:52 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y wget unzip xvfb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
-pip install -r requirements.txt
